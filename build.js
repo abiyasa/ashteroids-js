327bbfd74a8b728f49b88913d2e2ec3f801adcdb
abiyasa <ilham.suhardi@gmail.com>
2013-04-11 17:36:50 +0200

    fix indentation, make jshint happy

--- build.js
+++ build.js
@@ -1,38 +1,40 @@
 // see a complete list of options here:
 // https://github.com/jrburke/r.js/blob/master/build/example.build.js
+
+/* global requirejs */
 requirejs.config({
-  // all modules loaded are relative to this path
-  // e.g. require(["grid/core"]) would grab /lib/grid/core.js
-  baseUrl: ".",
+    // all modules loaded are relative to this path
+    // e.g. require(['grid/core']) would grab /lib/grid/core.js
+    baseUrl: '.',
 
-  // specify custom module name paths
-  paths: {
-    // JavaScript folders.
-    brejep: "lib/brejep",
-    utils: "lib/utils",
-    game: "src/game",
+    // specify custom module name paths
+    paths: {
+        // JavaScript folders.
+        brejep: 'lib/brejep',
+        utils: 'lib/utils',
+        game: 'src/game',
 
-    // Libraries
-    ash: "lib/ash/ash",
-    Stats: "lib/utils/Stats"
-  },
+        // Libraries
+        ash: 'lib/ash/ash',
+        Stats: 'lib/utils/Stats'
+    },
 
-  // Sets the configuration for your third party scripts that are not AMD compatible
-  shim: {
-    "Stats": {
-        "exports": "Stats"
-    }
-  },
+    // Sets the configuration for your third party scripts that are not AMD compatible
+    shim: {
+        'Stats': {
+            'exports': 'Stats'
+        }
+    },
 
-  // target amd loader shim as the main module, path is relative to baseUrl.
-  name: "lib/vendor/almond",
+    // target amd loader shim as the main module, path is relative to baseUrl.
+    name: 'lib/vendor/almond',
 
-  optimize: "none",
+    optimize: 'none',
 
-  // files to include along with almond.  only lib/skeleton.js is defined, as
-  // it pulls in the rest of the dependencies automatically.
-  include: [ "src/asteroids-app" ],
+    // files to include along with almond.  only lib/skeleton.js is defined, as
+    // it pulls in the rest of the dependencies automatically.
+    include: [ 'src/asteroids-app' ],
 
-  // build file destination, relative to the build file itself
-  out: "build/ashteroids.js"
+    // build file destination, relative to the build file itself
+    out: 'build/ashteroids.min.js'
 });
