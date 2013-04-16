48b5376e878389a3b29b0bfecaaf20a0145e9142
abiyasa <ilham.suhardi@gmail.com>
2013-04-16 23:00:04 +0200

    add util class to show screen/Views

---
+++ src/utils/ScreenManager.js
@@ -0,0 +1,41 @@
+/**
+ * Manages the displayed screens (Backbone Views)
+ */
+define([
+    'jquery',
+    'underscore',
+    'backbone'
+], function ($, _, Backbone) {
+    'use strict';
+
+    var ScreenManager = Backbone.View.extend({
+        /**
+         * Shows the specified screen. Will automatically remove the currently shown screen
+         */
+        showScreen: function (theScreen) {
+            // check if the current screen is being shown
+            if (this.currentView !== theScreen) {
+                this.clearScreen();
+                $(this.el).append(theScreen.render().el);
+
+                this.currentScreen = theScreen;
+            }
+
+            return this;
+        },
+
+        /**
+         * clear the currently shown screen
+         */
+        clearScreen: function () {
+            $(this.el).empty();
+
+            if (this.currentScreen) {
+                this.currentScreen.off();
+                this.currentScreen = undefined;
+            }
+        }
+    });
+
+    return ScreenManager;
+});
