d39b50d09e8fc3fc50cdf49a08678669233fc909
abiyasa <ilham.suhardi@gmail.com>
2013-05-01 16:56:26 +0200

    create spaceships & bullets using createJS

--- src/core/EntityCreator.js
+++ src/core/EntityCreator.js
@@ -1,3 +1,6 @@
+/**
+ * Big class for creating & destroying all game entities
+ */
 define([
     'ash',
     'game/components/asteroid',
@@ -49,18 +52,16 @@ define([
             this.game.removeEntity(entity);
         },
 
+        /**
+         * Creates asteroids using the given position & data
+         */
         createAsteroid: function(radius, x, y) {
             var asteroid = new Ash.Entity()
                 .add(new Asteroid())
                 .add(new Position(x, y, 0, radius))
-                .add(
-                    new Motion(
-                        (Math.random() - 0.5) * 4 * (50 - radius),
-                        (Math.random() - 0.5) * 4 * (50 - radius),
-                        Math.random() * 2 - 1,
-                        0
-                   )
-                );
+                .add(new Motion((Math.random() - 0.5) * 4 * (50 - radius),
+                    (Math.random() - 0.5) * 4 * (50 - radius),
+                    Math.random() * 2 - 1, 0));
 
             // create view based on game renderer
             var displayComponent;
@@ -73,12 +74,10 @@ define([
                 // TODO replace with real view
                 var g = new createjs.Graphics();
                 g.setStrokeStyle(1);
-                g.beginStroke(createjs.Graphics.getRGB(255, 255, 255));
-                g.beginFill(createjs.Graphics.getRGB(255, 128, 128));
+                g.beginStroke(createjs.Graphics.getRGB(0xFFFFFF, 1.0));
+                g.beginFill(createjs.Graphics.getRGB(0xFF8080, 1.0));
                 g.drawPolyStar(0, 0, radius, 5, 0.6, -90);
                 var shape = new createjs.Shape(g);
-                shape.x = 100;
-                shape.y = 100;
                 displayComponent = new CreateJSDisplay(shape);
                 break;
             }
@@ -87,10 +86,12 @@ define([
             }
 
             this.game.addEntity(asteroid);
-
             return asteroid;
         },
 
+        /**
+         * Creates the player's spaceship
+         */
         createSpaceship: function(x, y) {
             var spaceship = new Ash.Entity()
                 .add(new Spaceship())
@@ -98,12 +99,41 @@ define([
                 .add(new Motion(0, 0, 0, 15))
                 .add(new MotionControls(Keyboard.LEFT, Keyboard.RIGHT, Keyboard.UP, 100, 3))
                 .add(new Gun(8, 0, 0.3, 2))
-                .add(new GunControls(Keyboard.Z))
-                .add(new Display(new SpaceshipView(this.graphics)));
+                .add(new GunControls(Keyboard.Z));
+
+            // create view based on game renderer
+            var displayComponent;
+            switch (this.gameState.renderer) {
+            case this.gameState.RENDERER_CANVAS:
+                displayComponent = new Display(new SpaceshipView(this.graphics));
+                break;
+
+            case this.gameState.RENDERER_CREATE_JS:
+                // TODO replace with real view
+                var g = new createjs.Graphics();
+                g.setStrokeStyle(2);
+                g.beginStroke(createjs.Graphics.getRGB(0xFFFFFF, 1.0));
+                g.beginFill(createjs.Graphics.getRGB(0x80FFFF, 1.0));
+                g.moveTo(8, 0);
+                g.lineTo(-7, 7);
+                g.lineTo(-4, 0);
+                g.lineTo(-7, -7);
+                g.closePath();
+                var shape = new createjs.Shape(g);
+                displayComponent = new CreateJSDisplay(shape);
+                break;
+            }
+            if (displayComponent) {
+                spaceship.add(displayComponent);
+            }
+
             this.game.addEntity(spaceship);
             return spaceship;
         },
 
+        /**
+         * Creates spaceship bullet
+         */
         createUserBullet: function(gun, parentPosition) {
             var cos = Math.cos(parentPosition.rotation);
             var sin = Math.sin(parentPosition.rotation);
@@ -112,8 +142,28 @@ define([
                 .add(new Position(
                     cos * gun.offsetFromParent.x - sin * gun.offsetFromParent.y + parentPosition.position.x,
                     sin * gun.offsetFromParent.x + cos * gun.offsetFromParent.y + parentPosition.position.y, 0, 0))
-                .add(new Motion(cos * 150, sin * 150, 0, 0))
-                .add(new Display(new BulletView(this.graphics)));
+                .add(new Motion(cos * 150, sin * 150, 0, 0));
+
+            // create view based on game renderer
+            var displayComponent;
+            switch (this.gameState.renderer) {
+            case this.gameState.RENDERER_CANVAS:
+                displayComponent = new BulletView(this.graphics);
+                break;
+
+            case this.gameState.RENDERER_CREATE_JS:
+                // TODO replace with real view
+                var g = new createjs.Graphics();
+                g.beginFill(createjs.Graphics.getRGB(0xFFFFFF, 1.0));
+                g.arc(0, 0, 2, 0, Math.PI * 2, false);
+                var shape = new createjs.Shape(g);
+                displayComponent = new CreateJSDisplay(shape);
+                break;
+            }
+            if (displayComponent) {
+                bullet.add(displayComponent);
+            }
+
             this.game.addEntity(bullet);
             return bullet;
         }
