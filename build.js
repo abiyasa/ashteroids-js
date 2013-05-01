5c46615fe2500122d76c9e26f69a607e7b02c671
abiyasa <ilham.suhardi@gmail.com>
2013-05-01 17:27:45 +0200

    set base path to src

--- build.js
+++ build.js
@@ -5,27 +5,27 @@
 requirejs.config({
     // all modules loaded are relative to this path
     // e.g. require(['grid/core']) would grab /lib/grid/core.js
-    baseUrl: '.',
+    baseUrl: 'src',
 
     // specify custom module name paths
     paths: {
         // JavaScript folders.
-        utils: 'src/utils',
-        game: 'src/game',
-        core: 'src/core',
-        templates: 'templates',
+        utils: 'utils',
+        game: 'game',
+        core: 'core',
+        templates: '../templates',
 
         // Libraries
-        ash: 'lib/ash/ash',
-        stats: 'components/stats.js/src/Stats',
-        jquery: 'lib/utils/loader.jquery',
-        underscore: 'components/underscore/underscore',
-        backbone: 'components/backbone/backbone',
-        fillsnfixes: 'lib/utils/fillsnfixes',
-        easel: 'components/easeljs/lib/easeljs-0.6.0.min',
+        ash: '../lib/ash/ash',
+        stats: '../components/stats.js/src/Stats',
+        jquery: '../lib/utils/loader.jquery',
+        underscore: '../components/underscore/underscore',
+        backbone: '../components/backbone/backbone',
+        fillsnfixes: '../lib/utils/fillsnfixes',
+        easel: '../components/easeljs/lib/easeljs-0.6.0.min',
 
         // plugins
-        text: 'components/requirejs-text/text'
+        text: '../components/requirejs-text/text'
     },
 
     // Sets the configuration for your third party scripts that are not AMD compatible
@@ -49,14 +49,14 @@ requirejs.config({
     },
 
     // target amd loader shim as the main module, path is relative to baseUrl.
-    name: 'lib/vendor/almond',
+    name: '../lib/vendor/almond',
 
     optimize: 'none',
 
     // files to include along with almond.  only lib/skeleton.js is defined, as
     // it pulls in the rest of the dependencies automatically.
-    include: [ 'src/main-app' ],
+    include: [ 'main-app' ],
 
     // build file destination, relative to the build file itself
-    out: 'build/ashteroids.js'
+    out: '../build/ashteroids.js'
 });
