49622f9fd2222897faf8961eb9f376b19f3b0d95
abiyasa <ilham.suhardi@gmail.com>
2013-05-30 18:09:50 +0200

    able to select render mode from main screen
    still have problem in storing the last selected
    render mode

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -17,14 +17,20 @@ define([
         },
 
         initialize: function (config) {
-            // nothing to do yet
+            this.config = config || {};
         },
 
         render: function () {
             $(this.el).html(this.template());
 
             // activate carousel
-            this.$('.carousel').carousel();
+            if (!this.$carousel) {
+                this.$carousel = this.$('.carousel').carousel();
+            } else {
+                // TODO calling this the first time will fail
+                this.$carousel.data('carousel').to(this._getRenderModeIndex());
+            }
+
 
             return this;
         },
@@ -33,6 +39,7 @@ define([
             // check which button has been clicked
             switch (event.currentTarget.id) {
             case 'button-start':
+                this._updateSelectedRenderMode();
                 this.trigger('changeScreen', 'play');
                 break;
 
@@ -43,6 +50,53 @@ define([
             case 'button-about':
                 this.trigger('changeScreen', 'about');
                 break;
+            }
+        },
+
+        /**
+        * Internal array for available rendering modes
+        * TODO: create special helper classese
+        */
+        _availableRenderModes: {
+            'canvas': { index: 0 },
+            'createjs': { index: 1 },
+            'createjs-bitmap': { index: 2 },
+            'threejs': { index: 3 }
+        },
+
+        /**
+        * Internal function to help carousel selecting the active item based on
+        * the selected rendering mode
+        */
+        _getRenderModeIndex: function () {
+            var renderMode = this.config.renderMode || 'canvas';
+
+            var index;
+            if (this._availableRenderModes[renderMode]) {
+                index = this._availableRenderModes[renderMode].index;
+            } else {
+                index = 0;
+            }
+
+            return index;
+        },
+
+        /**
+        * Store the currently selected rendering mode based on active item on carousel
+        */
+        _updateSelectedRenderMode: function () {
+            var selectedIndex = 0;
+            if (this.$carousel) {
+                selectedIndex = this.$carousel.data('carousel').getActiveIndex();
+            }
+            selectedIndex = selectedIndex || 0;
+
+            for (var renderMode in this._availableRenderModes) {
+                var obj = this._availableRenderModes[renderMode];
+                if (obj.index === selectedIndex) {
+                    this.config.renderMode = renderMode;
+                    break;
+                }
             }
         }
     });
